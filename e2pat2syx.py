322c9589e8720c82579f19a8efcb73b69507291a
Bang Corrupt <bangcorrupt@gmail.com>
2021-08-07 15:27:21 +0100

    syx_enc, syx_dec

--- e2pat2syx.py
+++ e2pat2syx.py
@@ -1,71 +1,77 @@
 # Converts .e2pat to .syx
 import argparse
 from bitstring import BitArray
+
 def main():	
 	
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-p", "--pattern", dest='patNum', help="destination pattern number")
-	parser.add_argument("file", metavar="filepath", type=str, help="path/to/file.e2pat")
-	
-	args = parser.parse_args()
-	
-	with open(args.file, 'rb') as f:	
-		data =  bytearray(f.read())	
-	data = data[256:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pattern", dest='patNum', help="destination pattern number")
+    parser.add_argument("file", metavar="filepath", type=str, help="path/to/file.e2pat")
 
-	syxHead = bytearray([0xf0, 0x42, 0x30, 0x00, 0x01, 0x23, 0x40])		#	dump to current pattern
-	
-	if args.patNum:
-		patNum = int(args.patNum)-1
-		msb = 0
-		lsb = patNum % 128
-		if patNum > 127:
-			msb = 1
-		syxHead = bytearray([0xf0, 0x42, 0x30, 0x00, 0x01, 0x23, 0x4c, lsb, msb])	#	dump to patNum
-		
-	bits = BitArray(bytes=data).bin		# convert to bitstring
-	byt = []
-	b = []
-	for i in range(len(bits)):			# group into bytes
-		b.append(bits[i])
-		if len(b) == 8:
-			byt.append(b)
-			b = []
-	
-	sets = []
-	s = []
-	for i in range(len(byt)):			# group into 7 byte sets
-		s.append(byt[i])
-		if len(s) == 7:
-			sets.append(s)			
-			s = []
+    args = parser.parse_args()
 
-	nubyte = ['0']
-	for i in range(len(sets)):
-		for j in range(len(sets[i])):		
-			nubyte.insert(1,sets[i][j][0])	# add bit to new byte
-			sets[i][j][0] = '0'				# set bit to zero
-			if len(nubyte) == 8:		
-				sets[i].insert(0,nubyte)	# add new byte
-				nubyte = ['0']
-	
-	nudata = []
-	for s in sets:
-		for b in s:
-			nudata.append(int(''.join(b), 2))	# flatten list and convert bitstrings to int
+    with open(args.file, 'rb') as f:	
+        data =  bytearray(f.read())	
+    data = data[0x100:]
 
-	nudata = bytearray(nudata)					# convert to bytes
+    syxHead = bytearray([0xf0, 0x42, 0x30, 0x00, 0x01, 0x23, 0x40])		#	dump to current pattern
 
-	syxdata = syxHead + nudata +  bytearray([0x00,0x00,0x00,0x00,0x00,0xf7])	# Add sysex header and tail bytes
-	
-	outfile = args.file[:-5] + 'syx'											# change filename extension
-	with open(outfile, 'wb') as f:
-		f.write(syxdata)
+    if args.patNum:
+        patNum = int(args.patNum)-1
+        msb = 0
+        lsb = patNum % 128
+        if patNum > 127:
+            msb = 1
+        syxHead = bytearray([0xf0, 0x42, 0x30, 0x00, 0x01, 0x23, 0x4c, lsb, msb])	#	dump to patNum
+        
 
-	if args.patNum:
-		print(args.file + ' converted to ' + outfile + ', pattern number ' + args.patNum)
-	else:
-		print(args.file + ' converted to ' + outfile)
+    syxdata = syxHead + syx_enc(data) + b'\xf7'
+
+    outfile = args.file[:-5] + 'syx'											# change filename extension
+
+    with open(outfile, 'wb') as f:
+        f.write(syxdata)
+
+    if args.patNum:
+        print(args.file + ' converted to ' + outfile + ', pattern number ' + args.patNum)
+    else:
+        print(args.file + ' converted to ' + outfile)
+
+
+
+def syx_enc(byt):
+    
+    lng = len(byt)
+    lst = []
+    tmp = []
+    b = 0
+    cnt = 7
+    lim = 0
+    for i,e in enumerate(byt):
+
+        if lng < 7:
+            lim = 7 - lng
+
+        a = e & ~0b10000000
+        b |= ((e & 0b10000000)>>cnt)
+        
+        tmp.append(a)
+        
+        cnt -= 1
+        if cnt == lim:
+            lst.append([b])
+            lst.append(tmp)
+            tmp = []
+            b = 0
+            cnt = 7
+
+            if (lng - i) < 7:
+                lim = 7 - (lng - i) + 1
+
+    syx = [item for sublist in lst for item in sublist]
+
+    return bytes(syx)
+
 
 
 if __name__ == '__main__':
--- e2syx2pat.py
+++ e2syx2pat.py
@@ -3,72 +3,59 @@ import argparse
 from bitstring import BitArray
 
 def main():	
-	patHead = bytearray(b'\x4b\x4f\x52\x47\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x65\x6c\x65\x63\x74\x72\x69\x62\x65'
-							b'\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\xff\xff\xff\xff\xff'
-								b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-									b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-										b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-											b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-												b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-													b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-														b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-															b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-																b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-																	b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-																		b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-																			b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-																				b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-																					b'\xff\xff\xff\xff\xff\xff\xff')
-	parser = argparse.ArgumentParser()
-	parser.add_argument("file", metavar="filepath", type=str, help="path/to/file.syx")
-	
-	args = parser.parse_args()
 
-	with open(args.file, 'rb') as f:	
-		data =  bytearray(f.read())	
+    patHead = bytearray(b'\x4b\x4f\x52\x47' + b'\x00' * 12 + 
+                        b'\x65\x6c\x65\x63\x74\x72\x69\x62\x65' +
+                        b'\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00' + 
+                        b'\xff' * 220)
 
-	if data[6] == 0x40:						# remove syx header from 'current pattern dump'
-		data = data[7:]		
-	elif data[6] == 0x4c:					# remove syx header from 'pattern dump'
-		data = data[9:]
-	
-	bits = BitArray(bytes=data).bin			# convert to bitstring
-	byt = []
-	b = []
-	for i in range(len(bits)):				# group into bytes
-		b.append(bits[i])
-		if len(b) == 8:
-			byt.append(b)
-			b = []
-	
-	sets = []
-	s = []
-	for i in range(len(byt)):
-		s.append(byt[i])
-		if len(s) == 8: 					# group into 8 byte sets
-			sets.append(s)
-			s = []
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", metavar="filepath", type=str, help="path/to/file.syx")
 
-	for i in range(len(sets)):
-		syxbyte = sets[i][0][1:]			# get added byte
-		sets[i] = sets[i][1:]				# remove added byte from set
-		for j in range(len(sets[i])):
-			sets[i][j][0] = syxbyte[6-j]	# replace start bit
+    args = parser.parse_args()
 
-	nudata = []								
-	for s in sets:
-		for b in s:
-			nudata.append(int(''.join(b), 2))	# flatten list and convert bitstrings to int
+    with open(args.file, 'rb') as f:	
+        data =  bytearray(f.read())[:-1]
 
-	nudata = bytearray(nudata)					# convert to bytes
+    if data[6] == 0x40:						# remove syx header from 'current pattern dump'
+        data = data[7:]		
+    elif data[6] == 0x4c:					# remove syx header from 'pattern dump'
+        data = data[9:]
 
-	patData = patHead + nudata + bytearray([0x00,0x00,0x00,0x00])		# Add Korg file header and tail bytes
 
-	outfile = args.file[:-3] + 'e2pat'			# change filename extension
-	with open(outfile, 'wb') as f:
-		f.write(patData)
-	
-	print(args.file + ' converted to ' + outfile)
+    patData = patHead + syx_dec(data)
+
+    outfile = args.file[:-3] + 'e2pat'			# change filename extension
+    with open(outfile, 'wb') as f:
+        f.write(patData)
+
+    print(args.file + ' converted to ' + outfile)
+
+
+
+def syx_dec(syx):
+
+    chk = [syx[i:i + 8] for i in range(0, len(syx), 8)]
+
+    lst = []
+    tmp = []
+    a = 0
+    
+    for l in chk:
+        for i in range(len(l)-1):
+            a = l[i+1]
+            a |= ((l[0] & (1<<i))>>i)<<7
+            
+            tmp.append(a)
+
+        lst.append(tmp)
+        tmp = []
+    
+    byt = [item for sublist in lst for item in sublist]
+    
+    return bytes(byt)
+
 
 if __name__ == '__main__':
     main()
